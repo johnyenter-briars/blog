50189297349073a567f602355e766318767b5d77
John Yenter-Briars <jyenterbriars@gmail.com>
2021-10-27 08:52:12 -0500

    test

--- build_pull_submodules.sh
+++ build_pull_submodules.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+
 #build and pull newest frontend
+echo "pulling front end submodule"
 cd blog-frontend/
 git stash
 git checkout master
 git pull origin master
+echo "running npm install on frontend"
 npm install
 
 #build and pull newest theme
+echo "pulling theme submodule"
 cd themes/hexo-theme-cactus
 git stash
 git checkout master
@@ -15,14 +19,17 @@ git pull origin master
 
 cd ../../
 
+echo "running hexo generate"
 hexo generate
 
 cd ..
 
+echo "copying compiled frontend to root"
 #copy front over
 cp -r blog-frontend/public .
 
 #build rdp
+echo "pulling and building rdp"
 cd reverse-date-parser
 git stash
 git checkout master
@@ -32,5 +39,6 @@ npm run build
 cd ..
 
 #copy rdp over
+echo "copying over rdp"
 mkdir rdp
 cp -r reverse-date-parser/build/* rdp
