e4c7cb2a522265a793b80d5717242f2de61d895b
John Yenter-Briars <jyenterbriars@gmail.com>
2021-10-28 21:21:26 -0500

    tes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 #[cfg(test)]
 mod tests;
 
-use rocket::fs::{relative, FileServer};
+use std::net::SocketAddr;
+
+use rocket::{fs::{relative, FileServer}, get, request::FromRequest};
+
+
+
+
+#[get("/")]
+fn index(remote_addr: SocketAddr) -> String {
+    format!("Remote Address: {:?}", remote_addr)
+}
+
 
 #[rocket::launch]
 fn rocket() -> _ {
