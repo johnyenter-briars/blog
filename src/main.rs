c6b0545a7597a3867939a6419990c85f7d326df2
John Yenter-Briars <jyenterbriars@gmail.com>
2021-11-01 07:40:38 -0500

    update file access + rdp/blog udpates

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,12 @@
 mod tests;
 
 use rocket::fs::{relative, FileServer};
-use std::fs;
-use std::future::Future;
-use std::io::Cursor;
 use std::io::Write;
-use std::pin::Pin;
-use std::sync::atomic::{AtomicUsize, Ordering};
 
 use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::{ContentType, Method, Status};
 use rocket::{Data, Request, Response};
 
-use chrono::{self, Utc};
-
-// struct RuntimeData {
-//     start_time: chrono::DateTime<Utc>,
-// }
+use chrono;
 
 #[derive(Default)]
 struct RuntimeData {
@@ -53,19 +43,12 @@ impl Fairing for RuntimeData {
 
         use std::fs::OpenOptions;
 
-        let mut file = if let Ok(f) = OpenOptions::new()
-                    .write(true)
-                    .append(true)
-                    .create_new(true)
-                    .open(&path) {
-            f
-        } else {
-            fs::OpenOptions::new()
-                        .write(true)
-                        .append(true)
-                        .open(&path)
-                        .unwrap()
-        };
+        let mut file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .create(true)
+            .open(&path)
+            .unwrap();
 
         match file.write_all(string.as_bytes()) {
             Ok(_) => return,
