3432e2abdda59fd2ee2e6c400ae8e1cbcc9e7187
John Yenter-Briars <jyenterbriars@gmail.com>
2021-11-14 08:42:30 -0600

    fix nohup command

--- start_blog.sh
+++ start_blog.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cargo build --release
 
-PROFILE=release nohup ./target/release/jyb-blog & > jyb-blog.out 2> jyb-blog.err
+PROFILE=release nohup ./target/release/jyb-blog > jyb-blog.out 2> jyb-blog.err &
 
 echo $! > .procnum
